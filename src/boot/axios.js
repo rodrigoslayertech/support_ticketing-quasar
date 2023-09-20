feca764d515c797426bb697867f835d1ea1a1146
Rodrigo Vieira <rodrigo@slayer.tech>
2023-09-20 16:37:53 -0300

    🔧 Update axios configurations

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -7,7 +7,7 @@ import { LocalStorage } from 'quasar'
 const token = LocalStorage.getItem('bearer')
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api',
+  baseURL: 'http://localhost:8000/api/v1',
   headers: {
     Authorization: `Bearer ${token}`
   }
