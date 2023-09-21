b4f0c919b1f8e4ad90baa96b750f0ce882d8b744
Rodrigo Vieira <rodrigo@slayer.tech>
2023-09-20 22:46:53 -0300

    ✨ Added Ticket reply form / list

--- src/pages/TicketPage.vue
+++ src/pages/TicketPage.vue
@@ -1,35 +1,77 @@
 <template lang="pug">
-q-page(padding style="max-width: 1200px;")
-  <!--<img alt="Quasar logo" src="~assets/support_ticketing.logo.png" style="width: 200px; height: 200px">-->
+q-page(padding style="max-width: 1200px; margin: auto;")
+  q-scroll-area(
+    style="height: calc(100vh - 300px); width: 100%;"
+    ref="ticketScrollArea"
+  )
+    .row.q-mb-md
+      .col-md-11.col-sm-10.col-xs-10
+        div.text-caption Usuário: {{ ticket.user_id }}
+        div.text-h5 {{ ticket.title }}
+      .col-md-1.col-sm-2.col-xs-2.text-right
+        q-badge(v-if="ticket.status === 'opened'" color="red") aberto
+        q-badge(v-else-if="ticket.status === 'in_progress'") em progresso
+        q-badge(v-else-if="ticket.status === 'closed'" color="green") fechado
 
-  .row.q-mb-md
-    .col-11
-      div.text-caption Usuário: {{ ticket.user_id }}
-      div.text-h5 {{ ticket.title }}
-    .col-1.text-right
-      q-badge(v-if="ticket.status === 'opened'" color="red") aberto
-      q-badge(v-else-if="ticket.status === 'in_progress'") em progresso
-      q-badge(v-else-if="ticket.status === 'closed'" color="green") fechado
+    .row.q-mb-md
+      div {{ ticket.description }}
 
-  .row.q-mb-md
-    div {{ ticket.description }}
+    .row.justify-end
+      //-.col.text-left
+        .text-caption Ticket criado em:
+        .text-caption {{ ticket.created_at }}
+      .col.text-right(v-if="ticket.updated_at")
+        //-.text-caption Última atualização:
+        .text-caption {{ ticket.updated_at }}
 
-  .row.justify-end
-    .col.text-left
-      .text-caption Criado em:
-      .text-caption {{ ticket.created_at }}
-    .col.text-right(v-if="ticket.updated_at")
-      .text-caption Ultima atualização:
-      .text-caption {{ ticket.updated_at }}
+    q-separator(spaced="20px")
 
-  q-separator(spaced="20px")
+    div(v-for="reply in ticketReplies" :key="reply.id")
+      .row.q-mb-md
+        div {{ reply.reply }}
+      .row.justify-end
+      //-.col.text-left
+        .text-caption Ticket criado em:
+        .text-caption {{ ticket.created_at }}
+      .col.text-right(v-if="reply.updated_at")
+        //-.text-caption Última atualização:
+        .text-caption {{ reply.updated_at }}
+      q-separator(spaced="20px")
 
-  div {{ replies }}
+  q-form.q-gutter-md(
+    @submit.prevent.stop="replyTicket"
+    ref="ticketReplyForm"
+  )
+    q-input(
+      dense square outlined clearable
+      type="textarea" label="Digite uma resposta"
+      v-model="ticketReplyInput.reply"
+      :rules="ticketReplyRules.reply"
+    )
+      template(v-slot:prepend)
+        q-icon(name="quickreply")
+    //-q-input(
+      dense square outlined clearable
+      type="textarea" label="Digite uma descrição"
+      v-model="ticketInput.description"
+      :rules="ticketRules.description"
+      )
+      template(v-slot:prepend)
+        q-icon(name="description")
+    .row.justify-end
+      q-btn(
+        no-caps
+        @click="replyTicket"
+        label="Responder ticket" color="negative" icon="quickreply"
+        :loading="ticketReplyStatus.reply.loading" :disable="ticketReplyStatus.reply.loading"
+      )
+        template(v-slot:loading)
+          q-spinner-facebook
 </template>
 
 <script>
 import { defineComponent, ref } from 'vue'
-import { useRoute } from 'vue-router'
+import { useRoute, useRouter } from 'vue-router'
 
 import { useTicketStore } from 'src/stores/ticket'
 
@@ -38,10 +80,26 @@ export default defineComponent({
 
   setup () {
     const Route = useRoute()
+    const Router = useRouter()
     const Ticket = useTicketStore()
 
     const ticket = ref({})
-    const replies = ref([])
+    const ticketScrollArea = ref(null)
+    const ticketReplyForm = ref(null)
+    const ticketReplyInput = ref({
+      reply: ''
+    })
+    const ticketReplyRules = ref({
+      reply: [
+        value => (value && value.length > 0) || 'Digite uma resposta.'
+      ]
+    })
+    const ticketReplyStatus = ref({
+      reply: {
+        loading: false
+      }
+    })
+    const ticketReplies = ref([])
 
     const ticketId = Route.params.id
     Ticket.get(ticketId).then(data => {
@@ -65,31 +123,67 @@ export default defineComponent({
 
       ticket.value = data
     })
-    Ticket.getReplies(ticketId).then(data => {
-      const createdDate = new Date(data.created_at)
-      let updatedDate = null
-      if (data.created_at !== data.updated_at) {
-        updatedDate = new Date(data.updated_at)
-      } else {
-        data.updated_at = null
-      }
 
-      const dateCreated = createdDate.toLocaleDateString()
-      const timeCreated = createdDate.toLocaleTimeString()
-      data.created_at = `${dateCreated} às ${timeCreated}`
+    function getTicketReplies () {
+      Ticket.getReplies(ticketId).then(replies => {
+        replies.forEach(reply => {
+          const createdDate = new Date(reply.created_at)
+          const updatedDate = new Date(reply.updated_at)
 
-      if (updatedDate) {
-        const dateUpdated = updatedDate.toLocaleDateString()
-        const timeUpdated = updatedDate.toLocaleTimeString()
-        data.updated_at = `${dateUpdated} às ${timeUpdated}`
-      }
+          const dateCreated = createdDate.toLocaleDateString()
+          const timeCreated = createdDate.toLocaleTimeString()
+          reply.created_at = `${dateCreated} às ${timeCreated}`
 
-      replies.value = data
-    })
+          const dateUpdated = updatedDate.toLocaleDateString()
+          const timeUpdated = updatedDate.toLocaleTimeString()
+          reply.updated_at = `${dateUpdated} às ${timeUpdated}`
+        })
+
+        ticketReplies.value = replies
+      })
+    }
+    function replyTicket () {
+      ticketReplyStatus.value.reply.loading = true
+      ticketReplyForm.value.validate().then(passed => {
+        if (passed === false) {
+          ticketReplyStatus.value.reply.loading = false
+          return false
+        }
+
+        const Reply = {
+          ticket: ticketId,
+          ...ticketReplyInput.value
+        }
+        Ticket.sendReply(Reply).then(Reply => {
+          // listTicketReplies()
+
+          if (Reply.status === true) {
+            ticketReplyStatus.value.reply.loading = false
+            Router.go()
+          }
+        })
+      })
+
+      return true
+    }
+
+    getTicketReplies()
+
+    setTimeout(() => {
+      ticketScrollArea.value.setScrollPercentage('vertical', 1.0, 200)
+    }, 3000)
 
     return {
       ticket,
-      replies
+      ticketReplies,
+
+      ticketScrollArea,
+      ticketReplyForm,
+      ticketReplyInput,
+      ticketReplyRules,
+      ticketReplyStatus,
+
+      replyTicket
     }
   }
 })
--- src/stores/ticket.ts
+++ src/stores/ticket.ts
@@ -1,125 +1,208 @@
+import { ref } from 'vue'
 import { defineStore } from 'pinia'
 
 import { api } from 'boot/axios'
 
-export const useTicketStore = defineStore('ticket', {
-  state: () => ({
-    list: []
-  }),
+export const useTicketStore = defineStore('ticket', () => {
+  const tickets = ref([])
 
-  getters: {},
-
-  actions: {
-    async list () {
-      return await api.get('/ticket', {
-        withCredentials: true
-      }).then(response => {
-        if (!response.data) {
-          return false
-        }
-
-        const ticket = response.data
-
-        return ticket
-      }).catch(function (error) {
-        // Error 😨
-        if (error.response) {
-          /*
-          * The request was made and the server responded with a
-          * status code that falls out of the range of 2xx
-          */
-          console.info('Response Error - Data: ', error.response.data)
-          console.info('Response Error - Status: ', error.response.status)
-          console.info('Response Error - Headers: ', error.response.headers)
-        } else if (error.request) {
-          /*
-          * The request was made but no response was received, `error.request`
-          * is an instance of XMLHttpRequest in the browser and an instance
-          * of http.ClientRequest in Node.js
-          */
-          console.error('Request Error - XMLHttpRequest: ', error.request)
-        } else {
-          // Something happened in setting up the request and triggered an Error
-          console.error('Request Error (Unknown):', error)
-        }
-
+  async function list () {
+    return await api.get('/ticket', {
+      withCredentials: true
+    }).then(response => {
+      if (!response.data) {
         return false
-      })
-    },
-    async get (id:number) {
-      return await api.get('/ticket/' + id, {
-        withCredentials: true
-      }).then(response => {
-        if (!response.data) {
-          return false
-        }
+      }
 
-        this.list = response.data
+      const ticket = response.data
 
-        return this.list
-      }).catch(function (error) {
-        // Error 😨
-        if (error.response) {
-          /*
-          * The request was made and the server responded with a
-          * status code that falls out of the range of 2xx
-          */
-          console.info('Response Error - Data: ', error.response.data)
-          console.info('Response Error - Status: ', error.response.status)
-          console.info('Response Error - Headers: ', error.response.headers)
-        } else if (error.request) {
-          /*
-          * The request was made but no response was received, `error.request`
-          * is an instance of XMLHttpRequest in the browser and an instance
-          * of http.ClientRequest in Node.js
-          */
-          console.error('Request Error - XMLHttpRequest: ', error.request)
-        } else {
-          // Something happened in setting up the request and triggered an Error
-          console.error('Request Error (Unknown):', error)
-        }
+      return ticket
+    }).catch(function (error) {
+      // Error 😨
+      if (error.response) {
+        /*
+        * The request was made and the server responded with a
+        * status code that falls out of the range of 2xx
+        */
+        console.info('Response Error - Data: ', error.response.data)
+        console.info('Response Error - Status: ', error.response.status)
+        console.info('Response Error - Headers: ', error.response.headers)
+      } else if (error.request) {
+        /*
+        * The request was made but no response was received, `error.request`
+        * is an instance of XMLHttpRequest in the browser and an instance
+        * of http.ClientRequest in Node.js
+        */
+        console.error('Request Error - XMLHttpRequest: ', error.request)
+      } else {
+        // Something happened in setting up the request and triggered an Error
+        console.error('Request Error (Unknown):', error)
+      }
 
+      return false
+    })
+  }
+  async function get (id:number) {
+    return await api.get('/ticket/' + id, {
+      withCredentials: true
+    }).then(response => {
+      if (!response.data) {
         return false
-      })
-    },
-    async getReplies (id:number) {
-      return await api.get('/ticket/reply', {
-        withCredentials: true,
-        params: {
-          ticket: id
-        }
-      }).then(response => {
-        if (!response.data) {
-          return false
-        }
+      }
 
-        this.list = response.data
+      const ticket = response.data
 
-        return this.list
-      }).catch(function (error) {
-        // Error 😨
-        if (error.response) {
-          /*
-          * The request was made and the server responded with a
-          * status code that falls out of the range of 2xx
-          */
-          console.info('Response Error - Data: ', error.response.data)
-          console.info('Response Error - Status: ', error.response.status)
-          console.info('Response Error - Headers: ', error.response.headers)
-        } else if (error.request) {
-          /*
-          * The request was made but no response was received, `error.request`
-          * is an instance of XMLHttpRequest in the browser and an instance
-          * of http.ClientRequest in Node.js
-          */
-          console.error('Request Error - XMLHttpRequest: ', error.request)
-        } else {
-          // Something happened in setting up the request and triggered an Error
-          console.error('Request Error (Unknown):', error)
-        }
+      return ticket
+    }).catch(function (error) {
+      // Error 😨
+      if (error.response) {
+        /*
+        * The request was made and the server responded with a
+        * status code that falls out of the range of 2xx
+        */
+        console.info('Response Error - Data: ', error.response.data)
+        console.info('Response Error - Status: ', error.response.status)
+        console.info('Response Error - Headers: ', error.response.headers)
+      } else if (error.request) {
+        /*
+        * The request was made but no response was received, `error.request`
+        * is an instance of XMLHttpRequest in the browser and an instance
+        * of http.ClientRequest in Node.js
+        */
+        console.error('Request Error - XMLHttpRequest: ', error.request)
+      } else {
+        // Something happened in setting up the request and triggered an Error
+        console.error('Request Error (Unknown):', error)
+      }
 
+      return false
+    })
+  }
+  async function open (data: { title:string, description:string }) {
+    return await api.post('/ticket/', {
+      title: data.title,
+      description: data.description
+    }, {
+      withCredentials: true
+    }).then(response => {
+      if (!response.data) {
         return false
-      })
-    }
+      }
+
+      const ticket = response.data
+
+      return ticket
+    }).catch(function (error) {
+      // Error 😨
+      if (error.response) {
+        /*
+        * The request was made and the server responded with a
+        * status code that falls out of the range of 2xx
+        */
+        console.info('Response Error - Data: ', error.response.data)
+        console.info('Response Error - Status: ', error.response.status)
+        console.info('Response Error - Headers: ', error.response.headers)
+      } else if (error.request) {
+        /*
+        * The request was made but no response was received, `error.request`
+        * is an instance of XMLHttpRequest in the browser and an instance
+        * of http.ClientRequest in Node.js
+        */
+        console.error('Request Error - XMLHttpRequest: ', error.request)
+      } else {
+        // Something happened in setting up the request and triggered an Error
+        console.error('Request Error (Unknown):', error)
+      }
+
+      return false
+    })
+  }
+  async function getReplies (id:number) {
+    return await api.get('/ticket/reply', {
+      withCredentials: true,
+      params: {
+        ticket: id
+      }
+    }).then(response => {
+      if (!response.data) {
+        return false
+      }
+
+      const replies = response.data
+
+      return replies
+    }).catch(function (error) {
+      // Error 😨
+      if (error.response) {
+        /*
+        * The request was made and the server responded with a
+        * status code that falls out of the range of 2xx
+        */
+        console.info('Response Error - Data: ', error.response.data)
+        console.info('Response Error - Status: ', error.response.status)
+        console.info('Response Error - Headers: ', error.response.headers)
+      } else if (error.request) {
+        /*
+        * The request was made but no response was received, `error.request`
+        * is an instance of XMLHttpRequest in the browser and an instance
+        * of http.ClientRequest in Node.js
+        */
+        console.error('Request Error - XMLHttpRequest: ', error.request)
+      } else {
+        // Something happened in setting up the request and triggered an Error
+        console.error('Request Error (Unknown):', error)
+      }
+
+      return false
+    })
+  }
+  async function sendReply (reply: {ticket:number, reply:string, file:string}) {
+    return await api.post('/ticket/reply', {
+      ticket_id: reply.ticket,
+      reply: reply.reply,
+      file: reply.file
+    }, {
+      withCredentials: true
+    }).then(response => {
+      if (!response.data) {
+        return false
+      }
+
+      const result = response.data
+
+      return result
+    }).catch(function (error) {
+      // Error 😨
+      if (error.response) {
+        /*
+        * The request was made and the server responded with a
+        * status code that falls out of the range of 2xx
+        */
+        console.info('Response Error - Data: ', error.response.data)
+        console.info('Response Error - Status: ', error.response.status)
+        console.info('Response Error - Headers: ', error.response.headers)
+      } else if (error.request) {
+        /*
+        * The request was made but no response was received, `error.request`
+        * is an instance of XMLHttpRequest in the browser and an instance
+        * of http.ClientRequest in Node.js
+        */
+        console.error('Request Error - XMLHttpRequest: ', error.request)
+      } else {
+        // Something happened in setting up the request and triggered an Error
+        console.error('Request Error (Unknown):', error)
+      }
+
+      return false
+    })
+  }
+
+  return {
+    tickets,
+    list,
+    get,
+    open,
+    getReplies,
+    sendReply
   }
 })
